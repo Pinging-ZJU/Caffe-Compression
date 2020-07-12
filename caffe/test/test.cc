939e02ebcf023f1228b73d3d13ce3fbe677b9d41
Pinging-ZJU <zjuchenping@zju.com>
2020-07-12 07:15:33 +0000

    add all

---
+++ caffe/test/test.cc
@@ -0,0 +1,14 @@
+//#include <gflags/gflags.h>
+//#include <glog/logging.h>
+//#include<iostream>
+//
+////DEFINE_string(hosts, "127.0.0.1", "the server host");
+////DEFINE_int32(ports, 12306, "the server port");
+//
+//int main(int argc, char** argv) {
+//        google::ParseCommandLineFlags(&argc, &argv, true);
+//            std::string host = "127.0.0.1";
+//                int port =32;
+//                    LOG(INFO)<<"nihao";
+//                            std::cout << "The server host is: " << host
+//                                    << ", the server port is: " << port << std::endl;
